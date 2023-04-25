104049fd0ebfdccb160a065df941de014f92e7f3
Marcos Aurelio <126610050+MarkAurelio1@users.noreply.github.com>
2023-04-25 19:45:39 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Propriedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pagamento = new Pagamento();
+            pagamento.DataPagamento = DateTime.Now;
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Pagamento
+
+    {
+        public string NumeroBoleto;
+        public DateTime Vencimento { get; set; }
+
+        private DateTime _dataPagamento;
+
+        public DateTime DataPagamento
+        {
+            get { return _dataPagamento; }
+            set { _dataPagamento = value; }
+        }
+
+        void pagar() { }
+    }
+    public class Address
+    {
+        string CPF;
+    }
+}
+
+
+
+
+// Diferença entre Var e Propriedades
+// prop criar uma propriedade
+// Propriedades: Possue Get e Set são chamados de acessores,
+// pode atribuir ou receber valores atráves da var
